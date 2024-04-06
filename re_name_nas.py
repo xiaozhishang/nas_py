d4578bc044c01fcabd4877d655698832d494afcf
joe <xiaozhishang1108@outlook.com>
2024-04-06 21:44:43 +0800

    nas批量修改文件名脚本

---
+++ re_name_nas.py
@@ -0,0 +1,13 @@
+import  os
+import re
+
+file_path = r'/Volumes/share_vd/media/潜伏 (2009) 4K/'
+for root, dirs, files in os.walk(file_path):
+    # filenames.append(files)
+    print(files)
+
+for file in files:
+    srcFile = file
+    dstFile = re.sub(r'E(\d+)', r'S01E\1', srcFile)
+    # dstFile = re.sub(r'Fullmetal.Alchemist꞉ Brotherhood', r'钢之炼金术师', srcFile)
+    os.rename(file_path + '/' + srcFile, file_path + '/' +dstFile)
