042270ad8d1b82f5b7637c9e549c3f7490f5b3ae
joe <xiaozhishang1108@outlook.com>
2024-04-20 23:48:37 +0800

    路径改修

--- re_name_nas.py
+++ re_name_nas.py
@@ -1,12 +1,12 @@
 import os
 import re
 
-file_path = r'/Volumes/share_vd/media/东京女子图鉴 (2016)/Season 01/'
+file_path = r'/Volumes/share_vd/media/我们与恶的距离 (2019) 4K/'
 for root, dirs, files in os.walk(file_path):
     print(files)
 
 for file in files:
     srcFile = file
-    dstFile = re.sub(r'S(\d+)', r'东京女子图鉴S\1', srcFile)
+    dstFile = re.sub(r'第(\d+)', r'我们与恶的距离S01E\1', srcFile)
     # dstFile = re.sub(r'Fullmetal.Alchemist꞉ Brotherhood', r'钢之炼金术师', srcFile)
     os.rename(file_path + '/' + srcFile, file_path + '/' +dstFile)
